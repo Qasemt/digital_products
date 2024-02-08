c8b49351fbe922394f3dcb4d4a90b98db3dd9039
qasem <taaheri@outlook.com>
2024-02-08 14:56:17 +0330

    add recever

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.files.storage import default_storage
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import re
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    birthday = models.DateField()
-    mobile_number = models.CharField(max_length=20)
+    birthday = models.DateField(null=True)
+    mobile_number = models.CharField(max_length=20,)
     image = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    created_time = models.DateTimeField(_('create time'),auto_now_add=True)
+    updated_time = models.DateTimeField(_('update time'),auto_now=True)
 
+    
     def __str__(self):
         return self.user.username
-    
+
+    class Meta:
+        db_table = _('profiles')
+        verbose_name = _('Profile')
+        verbose_name_plural = _('Profiles')
+        ordering = ['last_name', 'name']
+
+
+    def clean(self):
+        # Validate the mobile_number field
+        if self.mobile_number:
+            pattern = r'^\+[0-9]{12}$'  # Regex pattern for the format +989359445555
+            if not re.match(pattern, self.mobile_number):
+                raise ValidationError('Mobile number should be in the format +989359445555')
+            
     #ّبرای حذف عکس قدیمی اگر عوض کرده باشد.
     def save(self, *args, **kwargs):
          
@@ -26,8 +49,14 @@ class Profile(models.Model):
 
         super().save(*args, **kwargs)
 
-    class Meta:
-        db_table = 'profiles'
-        verbose_name = 'Profile'
-        verbose_name_plural = 'Profiles'
-        ordering = ['last_name', 'name']
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
