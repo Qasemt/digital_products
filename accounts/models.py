d6d4bb19f58242965a71fc3d88765553af2efb51
qasem <taaheri@outlook.com>
2024-02-08 14:03:06 +0330

    حذف عکس قدیمی از مدیا

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.files.storage import default_storage
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
@@ -12,6 +12,20 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
     
+    #ّبرای حذف عکس قدیمی اگر عوض کرده باشد.
+    def save(self, *args, **kwargs):
+         
+        if self.pk:
+            
+            current_profile = Profile.objects.get(pk=self.pk)
+           
+            if current_profile.image and self.image != current_profile.image:
+                # Delete the old image from storage
+                if default_storage.exists(current_profile.image.name):
+                    default_storage.delete(current_profile.image.name)
+
+        super().save(*args, **kwargs)
+
     class Meta:
         db_table = 'profiles'
         verbose_name = 'Profile'
