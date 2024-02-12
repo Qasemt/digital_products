2d940199c2cfdac545dd83d3d3062767e7591746
qasem <taaheri@outlook.com>
2024-02-12 21:48:29 +0330

    change urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,17 @@
+from django.http import HttpResponse
 from django.urls import path
-from .views import ProfileAPIView, home_view, login_view, logout_view
+from .views import ChangePassword, ForgetPassword, ProfileAPIView, home_view, login_view, logout_view, register_view, user_verify
 
 urlpatterns = [
+    # path("", lambda request: HttpResponse("Hello, World!"), name="hello_world"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("register/", register_view, name="register"),
+    path("verify/<email_token>", user_verify, name="verify"),
+    path("password_reset/", home_view, name="password_reset"),
+    path("forget_password/", ForgetPassword, name="forget_password"),
+    path("change_password/<token>/", ChangePassword.as_view(), name="change_password"),
     path("accounts/profiles/", ProfileAPIView.as_view(), name="profile-list"),
     path("accounts/profile/<int:pk>", ProfileAPIView.as_view(), name="profile-list"),
+    path("", home_view, name="home"),
 ]
--- digital_products/urls.py
+++ digital_products/urls.py
@@ -8,17 +8,9 @@ from accounts.views import login_view, logout_view, home_view, register_view, Fo
 from digital_products.settings import BASE_DIR
 
 urlpatterns = [
-    path("", home_view, name="home"),
     path("admin/", admin.site.urls),
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
-    path("register/", register_view, name="register"),
-    path("verify/<email_token>", user_verify, name="verify"),
-    path("password_reset/", home_view, name="password_reset"),
-    path("forget_password/", ForgetPassword, name="forget_password"),
-    path("change_password/<token>/", ChangePassword.as_view(), name="change_password"),
-    # path("", include("accounts.urls")),
-    # path("", include("products.urls")),
+    path("", include("accounts.urls")),
+    path("", include("products.urls")),
 ]
 
 
