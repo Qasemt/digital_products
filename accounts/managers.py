ee7cad8b5d705295ca075758f9d1fdafcb3c462c
qasem <taaheri@outlook.com>
2024-02-16 13:28:06 +0330

    fix

--- accounts/managers.py
+++ accounts/managers.py
@@ -2,23 +2,23 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
 
-class CustomUserManager(BaseUserManager):
+# class CustomUserManager(BaseUserManager):
 
-    def create_user(self, email, password=None):
-        """
-        Create and save a user with the given email and password.
-        """
-        if not email:
-            raise ValueError(_("The Email must be set"))
-        email = self.normalize_email(email)
-        user = self.model(email=email)
-        user.set_password(password)
-        user.save()
-        return user
+#     def create_user(self, email, password=None):
+#         """
+#         Create and save a user with the given email and password.
+#         """
+#         if not email:
+#             raise ValueError(_("The Email must be set"))
+#         email = self.normalize_email(email)
+#         user = self.model(email=email)
+#         user.set_password(password)
+#         user.save()
+#         return user
 
-    def create_superuser(self, email, password=None):
-        SUPO = self.create_user(email, password)
-        SUPO.is_staff = True
-        SUPO.is_superuser = True
-        SUPO.save()
-        return SUPO
+#     def create_superuser(self, email, password=None):
+#         SUPO = self.create_user(email, password)
+#         SUPO.is_staff = True
+#         SUPO.is_superuser = True
+#         SUPO.save()
+#         return SUPO
--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,10 @@
+import re
 from django.db import models
 from django.core.files.storage import default_storage
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 from django.db.models.signals import post_save
-import re
 from django.contrib.auth.models import AbstractBaseUser
-from digital_products import settings
-from .managers import CustomUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
@@ -45,19 +43,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
     # REQUIRED_FIELDS = ["first_name", "last_name"]
 
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("The Email field must be set.")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        return self.create_user(email, password, **extra_fields)
 
 
 # ======== profile
